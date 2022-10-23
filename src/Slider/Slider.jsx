8f071adfa9e74a7f588facf02e5dd1d80187ee40
invinsibleho <invinsibleho369@gmail.com>
2022-10-23 22:39:45 +0600

    coachdata created

--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -7,45 +7,7 @@ import { Navigation } from "swiper";
 import { Box } from "@mui/system";
 import { Container, Divider, Typography } from "@mui/material";
 import Points from "../CoachSection/Points";
-
-// import images
-import slider1 from "../images/slider-2.jpg";
-import slider2 from "../images/slider-1jpg.jpg";
-import slider3 from "../images/slider-3.jpg";
-
-// coach profile
-const coachData = [
-  {
-    name: "Wen Tianxiang",
-    image: slider1,
-    profile: {
-      point1: "Ipsum has been the industry's standard dummy text ever",
-      point2: "To be able to teach effectively, the coach must have",
-      point3: "Displays Commitment and Clear Passion for the Sport",
-      point4: "Leads by Example with very High Attitude to Hard Work",
-    },
-  },
-  {
-    name: "Wen Tianxiang",
-    image: slider2,
-    profile: {
-      point1: "Understands the Sport and Leads by Example",
-      point2: "Sponge for Knowledge / Profound Thinker / Visionary",
-      point3: "Shares the Knowledge / Educates Others",
-      point4: "Highly Energised and a Motivator",
-    },
-  },
-  {
-    name: "Wen Tianxiang",
-    image: slider3,
-    profile: {
-      point1: "Knows the Athlete, Values and Respects that Relationship",
-      point2: "Is an Effective Communicator & Teacher",
-      point3: "Is a Good Listener",
-      point4: "Is Disciplined, Strong in Character and Integrity",
-    },
-  },
-];
+import coachData from "./coachData";
 
 const Slider = () => {
   return (
@@ -80,11 +42,9 @@ const Slider = () => {
               }}
               id="content"
             >
-              {/* coach name */}
               <Typography variant="h4" sx={{ mt: 3, mb: 4 }}>
                 {coach.name}
               </Typography>
-              {/* coach description */}
               <Points
                 point1={coach.profile.point1}
                 point2={coach.profile.point2}
